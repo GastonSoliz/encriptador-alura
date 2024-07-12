9676e2232e30844ec3d56937982d120f1ff66212
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-07-11 23:46:44 -0300

    adding dinamic responses to div result

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ document.addEventListener("DOMContentLoaded", function () {
     this.style.height = this.scrollHeight + "px";
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const autoExpandResult = document.getElementById("autoExpandResult");
+
+  const updateHeight = () => {
+    autoExpandResult.style.height = "auto";
+    autoExpandResult.style.height = autoExpandResult.scrollHeight + "px";
+  };
+
+  const observer = new MutationObserver(updateHeight);
+  observer.observe(autoExpandResult, {
+    childList: true,
+    subtree: true,
+    characterData: true,
+  });
+
+  updateHeight();
+});
