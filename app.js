70341a2a2886480317903aa168f5c56d0c3e5ab8
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-08-09 23:16:39 -0300

    js working

--- app.js
+++ app.js
@@ -1,3 +1,63 @@
+const inputText = document.querySelector(".input-text");
+const outputText = document.querySelector(".output-text");
+const encryptCode = [
+  ["e", "enter"],
+  ["i", "imes"],
+  ["a", "ai"],
+  ["o", "ober"],
+  ["u", "ufat"],
+];
+
+function encriptar(text) {
+  text = text.toLowerCase();
+  for (let i = 0; i < encryptCode.length; i++) {
+    if (text.includes(encryptCode[i][0])) {
+      text = text.replaceAll(encryptCode[i][0], encryptCode[i][1]);
+    }
+  }
+  return text;
+}
+
+function btnEncriptar() {
+  document.querySelector(".initial-result").style.display = "none";
+  // console.log(inputText.value);
+  const textEncrypted = encriptar(inputText.value);
+  outputText.textContent = textEncrypted;
+  document.querySelector(".final-result").style.display = "flex";
+  // console.log(textEncrypted);
+}
+
+function desencriptar(text) {
+  text = text.toLowerCase();
+  for (let i = 0; i < encryptCode.length; i++) {
+    if (text.includes(encryptCode[i][1])) {
+      text = text.replaceAll(encryptCode[i][1], encryptCode[i][0]);
+    }
+  }
+  return text;
+}
+
+function btnDesencriptar() {
+  document.querySelector(".initial-result").style.display = "none";
+  // console.log(inputText.value);
+  const textEncrypted = desencriptar(inputText.value);
+  outputText.textContent = textEncrypted;
+  document.querySelector(".final-result").style.display = "flex";
+  // console.log(textEncrypted);
+}
+
+document.getElementById("copyButton").addEventListener("click", function () {
+  const textToCopy = document.querySelector(".output-text").textContent;
+  navigator.clipboard
+    .writeText(textToCopy)
+    .then(() => {
+      alert("Texto copiado al portapapeles");
+    })
+    .catch((err) => {
+      alert("Error al copiar el texto: ", err);
+    });
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   const textarea = document.getElementById("autoExpand");
 
